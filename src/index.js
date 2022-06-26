200feaf75dec34338aa812c482f9984690947a4d
Francois Polo <andasan@gmail.com>
2022-06-26 14:01:32 -0700

    :sparkles: initializing a phaser configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-//
+import Phaser from 'phaser'
+
+//configuration for phaser
+const config = {
+  //webGL
+  type:Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    //arcade physics plugin, manages physics simulation
+    default: 'arcade'
+  },
+  scene: {
+    preload: preload,
+    create: create
+  }
+}
+
+//loading assets, such as images, music, animation, etc
+function preload(){
+  debugger;
+}
+
+//initializing the instances of the objects in the screen
+function create(){
+
+}
+
+new Phaser.Game(config);
