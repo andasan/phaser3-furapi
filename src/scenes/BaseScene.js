2afd38a4b99ec288822a0671d672eec7e313a81a
Francois Polo <andasan@gmail.com>
2022-06-27 16:14:02 -0700

    :alembic: added menu scene and items

--- src/scenes/BaseScene.js
+++ src/scenes/BaseScene.js
@@ -6,10 +6,24 @@ class BaseScene extends Phaser.Scene {
   constructor(key, config) {
     super(key);
     this.config = config;
+    this.screenCenter = [config.width/2, config.height/2]
+    this.fontSize = 32;
+    this.lineHeight = 42;
+    this.fontOptions = {fontSize: `${this.fontSize}px`, fill: '#CDFFFF'}
   }
 
   create() {
     this.add.image(0, 0, 'sky').setOrigin(0);
+  }
+
+  createMenu(menu){
+    let lastMenuPosY = 0;
+
+    menu.forEach(item => {
+        const menuPos = [this.screenCenter[0], this.screenCenter[1] + lastMenuPosY]
+        this.add.text(...menuPos, item.text, this.fontOptions).setOrigin(0.5, 1);
+        lastMenuPosY += this.lineHeight;
+    })
   }
 }
 
