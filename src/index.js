71ddcf6487f2e0f58ccd0ed85a79c3de36cfff10
Francois Polo <andasan@gmail.com>
2022-06-27 03:00:06 -0700

    :beers: refactor pipes

--- src/index.js
+++ src/index.js
@@ -21,12 +21,13 @@ const config = {
 const FLAP_VELOCITY = 250;
 const INIT_BIRD_POS = { x: config.width * 0.1, y: config.height / 2 };
 const PIPE_GAP_RANGE = [150, 250];
+const PIPE_X_GAP_RANGE = [400, 500];
 const PIPES_TO_RENDER = 4;
 const PIPE_X_GAP = 400;
 
 let bird = null;
-let upperPipe = null;
-let lowerPipe = null;
+let pipes = null;
+
 let pipeHorizontalGap = 0;
 
 function preload() {
@@ -37,22 +38,19 @@ function preload() {
 
 function create() {
   this.add.image(0, 0, "sky").setOrigin(0, 0);
-  bird = this.physics.add
-    .sprite(INIT_BIRD_POS.x, INIT_BIRD_POS.y, "bird")
-    .setOrigin(0.5);
+  bird = this.physics.add.sprite(INIT_BIRD_POS.x, INIT_BIRD_POS.y, "bird").setOrigin(0.5);
   bird.body.gravity.y = 400;
 
+  pipes = this.physics.add.group();
+
   for (let i = 0; i < PIPES_TO_RENDER; i++) {
-    pipeHorizontalGap += PIPE_X_GAP;
-    let pipeGap = Phaser.Math.Between(...PIPE_GAP_RANGE);
-    let pipePos = Phaser.Math.Between(0 + 20, config.height - 20 - pipeGap);
-
-    upperPipe = this.physics.add.sprite(pipeHorizontalGap, pipePos, "pipe").setOrigin(0, 1);
-    lowerPipe = this.physics.add.sprite(upperPipe.x, upperPipe.y + pipeGap, "pipe").setOrigin(0, 0);
-
-    upperPipe.body.velocity.x = -200;
-    lowerPipe .body.velocity.x = -200;
+    const upperPipe = pipes.create(0,0, "pipe").setOrigin(0, 1);
+    const lowerPipe = pipes.create(0,0, "pipe").setOrigin(0, 0);
+  
+    pipePlacement(upperPipe, lowerPipe)
   }
+
+  pipes.setVelocityX(-200);
 
   this.input.keyboard.on("keydown_SPACE", flap);
   this.input.on("pointerdown", flap);
@@ -70,8 +68,31 @@ function update(time, delta) {
   }
 }
 
+function pipePlacement(uPipe, lPipe){
+  const rightMostX = getNextPipe();
+  const pipeYGap = Phaser.Math.Between(...PIPE_GAP_RANGE);
+  const pipePos = Phaser.Math.Between(0 + 20, config.height - 20 - pipeYGap);
+  const pipeHorizontalGap = Phaser.Math.Between(...PIPE_X_GAP_RANGE)
+
+  uPipe.x = rightMostX + pipeHorizontalGap;
+  uPipe.y = pipePos;
+
+  lPipe.x = uPipe.x;
+  lPipe.y = uPipe.y + pipeYGap;
+}
+
 function flap() {
   bird.body.velocity.y = -FLAP_VELOCITY;
+}
+
+function getNextPipe(){
+  let rightMostX = 0;
+
+  pipes.getChildren().forEach(pipe => {
+    rightMostX = Math.max(pipe.x, rightMostX)
+  })
+
+  return rightMostX
 }
 
 function restartPosition() {
